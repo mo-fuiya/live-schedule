c3b3d6746e89d14ab16192f944807aa2e569cd1d
mo-fuiya <mrcp02380330@gmail.com>
2026-05-01 02:16:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
-document.addEventListener('DOMContentLoaded', async function () {
+eventClick: function(info) {
+  const title = info.event.title;
+  const date = info.event.startStr;
 
-  // JSON読み込み
-  const response = await fetch('data.json');
-  const data = await response.json();
-
-  // イベント変換
-  const events = data.events.map(e => ({
-    title: e.title,
-    start: e.date,
-    extendedProps: {
-      idolIds: e.idolIds
-    }
-  }));
-
-  // カレンダー生成
-  const calendar = new FullCalendar.Calendar(
-    document.getElementById('calendar'),
-    {
-      initialView: 'dayGridMonth',
-      events: events,
-
-      eventClick: function(info) {
-        const idolIds = info.event.extendedProps.idolIds;
-
-        // アイドル名に変換
-        const idolNames = idolIds.map(id => {
-          const idol = data.idols.find(i => i.id === id);
-          return idol ? idol.name : "不明";
-        });
-
-        alert(
-          info.event.title + "\n出演: " + idolNames.join(", ")
-        );
-      }
-    }
-  );
-
-  calendar.render();
-});
+  // URLに情報つけて移動
+  window.location.href = `event.html?title=${title}&date=${date}`;
+}
